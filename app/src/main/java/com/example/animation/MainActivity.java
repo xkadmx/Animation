28f2cd5c631b11054ec00de3d82fbb836be18632
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-03 21:38:33 +0200

    AlphaAnimation()

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.animation;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    ImageView malaysia;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        malaysia = (ImageView) findViewById(R.drawable.malaysia);
+    }
+    private void startAnimation(){
+        Animation alpha = new AlphaAnimation()
     }
 }
